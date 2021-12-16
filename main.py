a8e35c20e04a893159526901a99f7eaa5877805f
Sergei Garibzyanov <garibzyanovsergei@gmail.com>
2021-12-16 21:42:47 +0500

    Optimize functions

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from time import sleep
 
 print('Добро пожаловать в приложение "Шифр Цезаря"')
-sleep(2)
+sleep(1.5)
 
 cipher = '0'
 alphabet = '0'
@@ -75,9 +75,7 @@ elif alphabet == '2':
                 flag = True
                 break
 
-def encrypt_eng():
-    start_alphabet = ord('a') - 1
-    end_alphabet = ord('z')
+def encrypt():
     lower_symbol_set = symbol_set.lower()
     for i in range(len(lower_symbol_set)):
         sym_num = ord(lower_symbol_set[i])
@@ -93,45 +91,7 @@ def encrypt_eng():
         else:
             print(symbol_set[i], end='')
 
-def encrypt_rus():
-    start_alphabet = ord('а') - 1
-    end_alphabet = ord('я')
-    lower_symbol_set = symbol_set.lower()
-    for i in range(len(lower_symbol_set)):
-        sym_num = ord(lower_symbol_set[i])
-        if start_alphabet < sym_num <= end_alphabet:
-            encrypt_sym_num = sym_num + int(step)
-            if encrypt_sym_num > end_alphabet:
-                remainder = encrypt_sym_num - end_alphabet
-                encrypt_sym_num = start_alphabet + remainder
-            if symbol_set[i].isupper():
-                print((chr(encrypt_sym_num)).upper(), end='')
-            else:
-                print(chr(encrypt_sym_num), end='')
-        else:
-            print(symbol_set[i], end='')
-
-def decrypt_eng():
-    start_alphabet = ord('a')
-    end_alphabet = ord('z') + 1
-    lower_symbol_set = symbol_set.lower()
-    for i in range(len(lower_symbol_set)):
-        sym_num = ord(lower_symbol_set[i])
-        if start_alphabet <= sym_num < end_alphabet:
-            decrypt_sym_num = sym_num - int(step)
-            if decrypt_sym_num < start_alphabet:
-                remainder = start_alphabet - decrypt_sym_num
-                decrypt_sym_num = end_alphabet - remainder
-            if symbol_set[i].isupper():
-                print((chr(decrypt_sym_num)).upper(), end='')
-            else:
-                print(chr(decrypt_sym_num), end='')
-        else:
-            print(symbol_set[i], end='')
-
-def decrypt_rus():
-    start_alphabet = ord('а')
-    end_alphabet = ord('я') + 1
+def decrypt():
     lower_symbol_set = symbol_set.lower()
     for i in range(len(lower_symbol_set)):
         sym_num = ord(lower_symbol_set[i])
@@ -148,10 +108,18 @@ def decrypt_rus():
             print(symbol_set[i], end='')
 
 if cipher == '1' and alphabet == '1':
-    encrypt_eng()
+    start_alphabet = ord('a') - 1
+    end_alphabet = ord('z')
+    encrypt()
 elif cipher == '1' and alphabet == '2':
-    encrypt_rus()
+    start_alphabet = ord('а') - 1
+    end_alphabet = ord('я')
+    encrypt()
 if cipher == '2' and alphabet == '1':
-    decrypt_eng()
+    start_alphabet = ord('a')
+    end_alphabet = ord('z') + 1
+    decrypt()
 elif cipher == '2' and alphabet == '2':
-    decrypt_rus()
+    start_alphabet = ord('а')
+    end_alphabet = ord('я') + 1
+    decrypt()
